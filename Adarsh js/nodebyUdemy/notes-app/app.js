04f2c13d80b0febfb83edafc085e74d7ae5cd2d3
Adarsh <adarshvasu90@gmail.com>
2023-02-13 16:08:49 +0530

    final commit

--- Adarsh js/nodebyUdemy/notes-app/app.js
+++ Adarsh js/nodebyUdemy/notes-app/app.js
@@ -1,7 +1,70 @@
-const fs = require('fs')
-// const append =require(fs.appendFileSync)
-// fs.writeFileSync('notes.txt','My name is adarsh')
+const chalk =require('chalk')
+const { argv } = require('yargs')
+const yargs =require('yargs')
+const add =require('./notes')
+
+//Customize yargs
+yargs.version('1.1.0')
+
+//Create add command
+yargs.command({
+    command:'Add',
+    describe:'Add a new Note',
+    builder:{
+        title:{
+            describe:'Note Title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'Body Title',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(){
+        console.log('Title: ',argv.title)
+        console.log('Body: '+argv.body)
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'remove a node',
+    handler:function(){
+        console.log("Removing the note");
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe:'reading the file',
+    handler:function(){
+        console.log("reading the file ")
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:"list the file",
+    handler:function(){
+        console.log("listing the file")
+    }
+})
 
 
-//append a message to notes.txt
-fs.appendFileSync('notes.txt',' I live in Indore'); 
+// another way to write the c.l
+yargs.parse(); 
+// console.log(yargs.argv)
+
+
+
+// const commmand =process.argv[2]
+// console.log(process.argv)
+
+// if(commmand === 'add'){
+//     console.log("Adding notes")
+// }
+// else if(commmand =='remove'){
+//     console.log('Removing note!')
+// }
